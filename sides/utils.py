d543d2d8f67c139b064b1ed9c9abe2610d07d994
Dang <louidang@amazon.com>
2016-07-31 10:39:27 +1000

    Change constraints of the LP model to make sure all students are evacuated

--- sides/utils.py
+++ sides/utils.py
@@ -1,8 +1,6 @@
 import googlemaps
 gmaps = googlemaps.Client(key='AIzaSyDPvDQc0_i1cT9sMoT7vnHRUuk8vF3D1CE')
 from pulp import LpProblem, LpMinimize, LpInteger, LpVariable, lpSum
-import os
-import csv
 import collections
 import logging
 
@@ -10,69 +8,72 @@ logging.basicConfig()
 log = logging.getLogger(__name__)
 
 def convert_int(x):
-	try:
-		return int(float(x))
-	except ValueError:
-		raise
+    try:
+        return int(float(x))
+    except ValueError:
+        raise
 
-
-# schools = collections.OrderedDict()
-# for row in csv.DictReader(open(os.path.expanduser('~/workbook1.csv'))):
-# 	try:
-# 		schools[row['SCHOOL_NAME']] =\
-# 			{'id': row['SCHOOL_NAME'],
-# 			'lat': float(row['LATITUDE']),
-# 			'lng': float(row['LONGITUDE']),
-# 			'num_students': convert_int(row['TOTAL_ENROLMENTS'])}
-# 	except ValueError:
-# 		continue
-
-# sources = schools.values()[:3]
-# destinations = schools.values()[4:10]
 
 def get_optimal_routes(sources, destinations):
-	sources = collections.OrderedDict([(x['id'], x) for x in sources])
-	destinations = collections.OrderedDict([(x['id'], x) for x in destinations])
+    sources = collections.OrderedDict([(x['id'], x) for x in sources])
+    destinations = collections.OrderedDict([(x['id'], x) for x in destinations])
 
-	sources_points = [{'lat': x['lat'], 'lng': x['lng']} for x in sources]
-	destinations_points = [{'lat': x['lat'], 'lng': x['lng']} for x in destinations]
+    sources_points = [{'lat': x['lat'], 'lng': x['lng']} for x in sources.values()]
+    destinations_points = [{'lat': x['lat'], 'lng': x['lng']} for x in destinations.values()]
 
-	source_ids = [x['id'] for x in sources]
-	dest_ids = [x['id'] for x in destinations]
+    source_ids = [x['id'] for x in sources.values()]
+    dest_ids = [x['id'] for x in destinations.values()]
 
-	demand = {x['id']: x['num_students'] for x in sources}
-	supply = {x['id']: x['num_students'] for x in destinations}
+    demand = {x['id']: x['num_students'] for x in sources.values()}
+    supply = {x['id']: x['num_students'] for x in destinations.values()}
 
-	log.info("Calling gmaps api...")
-	distances = gmaps.distance_matrix(origins=sources_points, destinations=destinations_points, mode='walking')
+    log.info("Calling gmaps api...")
+    distances = gmaps.distance_matrix(origins=sources_points, destinations=destinations_points, mode='walking')
 
-	costs = {}
-	for i, origin in enumerate(distances['rows']):
-		origin_costs = {}
-		for j, entry in enumerate(origin['elements']):
-			origin_costs[dest_ids[j]] = entry['duration']['value']
-		costs[source_ids[i]] = origin_costs
+    costs = {}
+    for i, origin in enumerate(distances['rows']):
+        origin_costs = {}
+        for j, entry in enumerate(origin['elements']):
+            origin_costs[dest_ids[j]] = entry['duration']['value']
+        costs[source_ids[i]] = origin_costs
 
-	prob = LpProblem("Evaucation Routing for Schools",LpMinimize)
-	routes = [(s,d) for s in source_ids for d in dest_ids]
-	route_lookup = {'Route_{}_{}'.format(x.replace(' ','_'),y.replace(' ','_')):(x,y) for (x,y) in routes}
-	route_vars = LpVariable.dicts("Route",(source_ids,dest_ids),0,None,LpInteger)
-	prob += lpSum([route_vars[w][b]*costs[w][b] for (w,b) in routes])
-	# The supply maximum constraints are added to prob for each supply node (warehouse)
-	for dest in dest_ids:
-	    prob += lpSum([route_vars[source][dest] for source in source_ids]) <= supply[dest], "Students going to %s" % dest
-	for source in source_ids:
-	    prob += lpSum([route_vars[source][dest] for dest in dest_ids]) >= demand[source], "Students leaving %s"%source
+    prob = LpProblem("Evaucation Routing for Schools",LpMinimize)
+    routes = [(s,d) for s in source_ids for d in dest_ids]
+    route_lookup = {'Route_{}_{}'.format(x.replace(' ','_'),y.replace(' ','_')):(x,y) for (x,y) in routes}
+    route_vars = LpVariable.dicts("Route",(source_ids,dest_ids),0,None,LpInteger)
+    prob += lpSum([route_vars[w][b]*costs[w][b] for (w,b) in routes])
+    for dest in dest_ids:
+        prob += lpSum([route_vars[source][dest] for source in source_ids]) <= supply[dest], "Students going to {} is <= {}".format(demand, supply[dest])
+    for source in source_ids:
+        prob += lpSum([route_vars[source][dest] for dest in dest_ids]) == demand[source], "Students leaving {} is {}".format(source, demand[source])
 
-	log.info("Optimizing routes...")
-	prob.solve()
+    log.info("Optimizing routes...")
+    prob.solve()
 
-	if prob.status != 1:
-		raise Exception("Algorithm could not converge to a solution")
+    if prob.status != 1:
+        raise Exception("Algorithm could not converge to a solution")
 
-	result = []
-	for v in prob.variables():
-		src, dst = route_lookup[v.name]
-		value = v.value()
-		result.append({'src': sources[src], 'dst': destinations[dst], 'value': int(value)})
-	return result
+    result = []
+    for v in prob.variables():
+        src, dst = route_lookup[v.name]
+        value = v.value()
+        result.append({'src': sources[src], 'dst': destinations[dst], 'value': int(value)})
+    return result
+
+if __name__ == '__main__':
+    import csv
+    import os
+    schools = collections.OrderedDict()
+    for row in csv.DictReader(open(os.path.expanduser('~/workbook1.csv'))):
+        try:
+            schools[row['SCHOOL_NAME']] =\
+                {'id': row['SCHOOL_NAME'],
+                'lat': float(row['LATITUDE']),
+                'lng': float(row['LONGITUDE']),
+                'num_students': convert_int(row['TOTAL_ENROLMENTS'])}
+        except ValueError:
+            continue
+
+    sources = schools.values()[:3]
+    destinations = schools.values()[4:10]
+    results = get_optimal_routes(sources, destinations)
