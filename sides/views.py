e3cddb05aa898ebb6fbe4925faee32788031921d
Dang <louidang@amazon.com>
2016-07-31 12:57:06 +1000

    Catch server errors

--- sides/views.py
+++ sides/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseBadRequest, JsonResponse, HttpResponse
+from django.http import HttpResponseBadRequest, JsonResponse, HttpResponse, HttpResponseServerError
 from sides import utils
 import json
 import logging
@@ -6,20 +6,22 @@ import logging
 log = logging.getLogger(__name__)
 
 def get_optimal_routes(request):
-    if request.method=='POST':
-    	#if 'data' not in request.POST:
-    		#return HttpResponseBadRequest("No input data received")
-        #received_json_data=json.loads(request.POST['data'])
-        log(request.POST)
-        if 'data' in request.POST:
-        	log(request.POST['data'])
-        if 'sources' not in request.POST:
-        	return HttpResponseBadRequest("No sources")
-        if 'destinations' not in request.POST:
-        	return HttpResponseBadRequest("No destinations")
+	try:
+	    if request.method=='POST':
+	    	#if 'data' not in request.POST:
+	    		#return HttpResponseBadRequest("No input data received")
+	        #received_json_data=json.loads(request.POST['data'])
+	        log(request.POST)
+	        if 'data' in request.POST:
+	        	log(request.POST['data'])
+	        if 'sources' not in request.POST:
+	        	return HttpResponseBadRequest("No sources")
+	        if 'destinations' not in request.POST:
+	        	return HttpResponseBadRequest("No destinations")
 
-        result = utils.get_optimal_routes(request.POST['sources'], (request.POST['destinations']))
-    	return JsonResponse(result)
-    else:
-        return HttpResponseBadRequest("Only accepts POST for now")
-
+	        result = utils.get_optimal_routes(request.POST['sources'], (request.POST['destinations']))
+	    	return JsonResponse(result)
+	    else:
+	        return HttpResponseBadRequest("Only accepts POST for now")
+	except Exception as e:
+		return HttpResponseServerError("Server erro: {}".format(e))
