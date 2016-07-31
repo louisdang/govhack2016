f75d446205e7de47aeb49269d756e01a87ef4c6d
Dang <louidang@amazon.com>
2016-07-31 10:04:33 +1000

    Add JSONP support for get_optimal_routes endpoint

--- sides/views.py
+++ sides/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseBadRequest, JsonResponse
+from django.http import HttpResponseBadRequest, JsonResponse, HttpResponse
 from sides import utils
 import json
 
@@ -8,7 +8,12 @@ def get_optimal_routes(request):
     		return HttpResponseBadRequest("No input data received")
         received_json_data=json.loads(request.GET['data'])
         result = utils.get_optimal_routes(received_json_data['sources'], received_json_data['destinations'])
-        return JsonResponse(result)
+        if 'callback' in request.REQUEST:
+                # a jsonp response!
+                resp_text = '%s(%s);' % (request.REQUEST['callback'], json.dumps(result))
+            	return HttpResponse(resp_text, "text/javascript")
+        else:
+        	return JsonResponse(result)
     else:
         return HttpResponseBadRequest("Only accepts GET for now")
 
