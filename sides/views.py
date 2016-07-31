a919dfb58a5c78d7e262b77a04a0b3398d564c59
Dang <louidang@amazon.com>
2016-07-31 12:55:57 +1000

    Add logging

--- sides/views.py
+++ sides/views.py
@@ -1,16 +1,23 @@
 from django.http import HttpResponseBadRequest, JsonResponse, HttpResponse
 from sides import utils
 import json
+import logging
+
+log = logging.getLogger(__name__)
 
 def get_optimal_routes(request):
     if request.method=='POST':
     	#if 'data' not in request.POST:
     		#return HttpResponseBadRequest("No input data received")
         #received_json_data=json.loads(request.POST['data'])
+        log(request.POST)
+        if 'data' in request.POST:
+        	log(request.POST['data'])
         if 'sources' not in request.POST:
         	return HttpResponseBadRequest("No sources")
         if 'destinations' not in request.POST:
         	return HttpResponseBadRequest("No destinations")
+
         result = utils.get_optimal_routes(request.POST['sources'], (request.POST['destinations']))
     	return JsonResponse(result)
     else:
